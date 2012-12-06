536ed12cb2732329792eae2c004ba96090f8e30c
ewheeler <evanmwheeler@gmail.com>
2012-12-06 08:50:40 -0600

    add fonts and google analytics

--- page.rb
+++ page.rb
@@ -128,11 +128,14 @@ class HTMLGen
                 self.meta(:charset => "utf-8")+
                 self.title{H.entities @title}+
                 self.meta(:content => :index, :name => :robots)+
+		self.link(:href => "http://fonts.googleapis.com/css?family=Open+Sans|Inconsolata",
+			  :rel => "stylesheet", :type => "text/css")+
+                self.link(:href => "/images/favicon.ico", :rel => "shortcut icon")+
                 self.link(:href => "/css/style.css?v=10", :rel => "stylesheet",
                           :type => "text/css")+
-                self.link(:href => "/images/favicon.png", :rel => "shortcut icon")+
-                self.script(:src => "/js/jquery.1.6.4.min.js"){}+
-                self.script(:src => "/js/app.js?v=10"){}
+                self.script(:type => "text/javascript", :src => "/js/jquery.1.6.4.min.js"){}+
+                self.script(:type => "text/javascript", :src => "/js/app.js?v=10"){}+
+		self.script(:type => "text/javascript"){"var _gaq = _gaq || []; var pluginUrl ='//www.google-analytics.com/plugins/ga/inpage_linkid.js';_gaq.push(['_require', 'inpage_linkid', pluginUrl]);_gaq.push(['_setAccount', 'UA-36855405-1']); _gaq.push(['_trackPageview']); (function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();"}
             }+
             self.body {
                 self.div(:class => "container") {
